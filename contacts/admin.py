3eaabc8a449caf4bf35e01a907a05b24cc8ad42f
TaMinh <taminhta2003@gmail.com>
2024-08-02 13:53:05 +0700

    Add ServiceContact and Solution Contact to CMS

--- contacts/admin.py
+++ contacts/admin.py
@@ -12,13 +12,24 @@
 # admin.site.register(Contact, ContactsAdmin)
 from django.contrib import admin
 from admin_list_charts.admin import ListChartMixin
-from .models import Contact
+from .models import Contact, ServiceContact, SolutionContact
 
 class ContactAdmin(ListChartMixin,admin.ModelAdmin):
     list_display = ["name", "email", "message", "date_sent"]
     date_hierarchy = "date_sent"
     search_fields = ["name","email","message","date_sent"]
-    # change_list_template = "admin/contacts_list_admin.html"
-
-    
 admin.site.register(Contact, ContactAdmin)
+
+class ServiceContactAdmin(ListChartMixin,admin.ModelAdmin):
+    list_display = ["name", "email","service", "message", "date_sent"]
+    date_hierarchy = "date_sent"
+    search_fields = ["name","email","message","date_sent"]
+    list_filter = ['service',]
+admin.site.register(ServiceContact, ServiceContactAdmin)
+
+class SolutionContactAdmin(ListChartMixin,admin.ModelAdmin):
+    list_display = ["name", "email","solution", "message", "date_sent"]
+    date_hierarchy = "date_sent"
+    search_fields = ["name","email","message","date_sent"]  
+    list_filter = ["solution"]
+admin.site.register(SolutionContact, SolutionContactAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from services.models import Service
+from solutions.models import Solution
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
@@ -8,6 +10,32 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} at {self.date_sent}"
+
+    class Meta:
+        ordering = ['-date_sent']  # Orders messages with the most recent first
+
+class SolutionContact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    date_sent = models.DateTimeField(auto_now_add=True)  # Automatically set to the current date and time when the message is created
+    solution = models.ForeignKey(Solution, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Message from {self.name} at {self.date_sent} about {self.solution}"
+
+    class Meta:
+        ordering = ['-date_sent']  # Orders messages with the most recent first
+
+class ServiceContact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    date_sent = models.DateTimeField(auto_now_add=True)  # Automatically set to the current date and time when the message is created
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Message from {self.name} at {self.date_sent} about {self.service}"
 
     class Meta:
         ordering = ['-date_sent']  # Orders messages with the most recent first
