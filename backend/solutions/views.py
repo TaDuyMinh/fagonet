ff3519c973381aa99fb953765f8d3de594df8cc1
TaMinh <taminhta2003@gmail.com>
2024-08-03 19:52:09 +0700

    optimize solutions view

--- backend/solutions/views.py
+++ backend/solutions/views.py
@@ -7,9 +7,3 @@ class SolutionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Solution.objects.filter(is_active=True)  # Filter based on your requirements
     serializer_class = SolutionSerializer
 
-class SolutionGalleryViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = SolutionGallerySerializer
-
-    def get_queryset(self):
-        solution_id = self.kwargs.get('solution_id')
-        return SolutionGallery.objects.filter(solution_id=solution_id)
